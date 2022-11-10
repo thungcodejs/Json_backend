238c3c37f5cf93569b8f0dfc29726a7c06eb39ae
thungcodejs <ptuanhungg@gmail.com>
2022-11-10 13:52:45 +0700

    back-end json

---
+++ models/order-item.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose');const orderItemSchema = mongoose.Schema({    quantity: {        type: Number,        required: true    },    dish: {        type: mongoose.Schema.Types.ObjectId,        ref: 'Dish',        // required: true    }})exports.OrderItem = mongoose.model('OrderItem', orderItemSchema);
